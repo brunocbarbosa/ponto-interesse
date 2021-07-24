16a9ebb930889b8bd2f887636832f8f1ef7c6509
Bruno Barbosa <bruno.cbarbosa90@gmail.com>
2021-07-24 13:14:38 -0300

    feat: created middleware errors, to treat the errors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import 'express-async-errors';
+
 import { router } from './router';
+import { errors } from './middlewares/errors'
 
 const app = express()
 app.use(express.json());
 app.use(router);
+app.use(errors);
 
 app.listen(3000, () => console.log("Server running!!"))
